1db9a4d4c079e1ea85a715559b9693f5320b988a
crdchip <quocson16051997@gmail.com>
2024-10-25 03:37:25 +0700

    2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -61,9 +61,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "68577746bd9a058b692e12b319af9dcf
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c5cfe3f7260f18140df6875b40669e8c",
-"/": "c5cfe3f7260f18140df6875b40669e8c",
-"main.dart.js": "83fa611febeedcd9c43de2e6e72362ab",
+"index.html": "c96f5c80be1a9f2979090850daeacd5c",
+"/": "c96f5c80be1a9f2979090850daeacd5c",
+"main.dart.js": "ecd8faf4a80501e8d194bb29beaa20f6",
 "manifest.json": "8b189eba7903cfc53fac674a7017ef28",
 "version.json": "8db8d11e79814103bce68b172072e923"};
 // The application shell files that are downloaded before a service worker can
